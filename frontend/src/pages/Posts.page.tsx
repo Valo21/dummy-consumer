f1d36ae19c1d610d8d75570f1a901ea64ffda067
Valentín <61363314+Valo21@users.noreply.github.com>
2024-07-10 13:19:16 -0300

    feat(frontend): :sparkles: add posts search bar

--- frontend/src/pages/Posts.page.tsx
+++ frontend/src/pages/Posts.page.tsx
@@ -12,6 +12,30 @@ function PostsList({ posts }: { posts: IPost[] }) {
     )
 }
 
+interface SearchBarProps {
+    onSubmit: (params: string) => void
+}
+
+function SearchBar({ onSubmit }: SearchBarProps) {
+    const [input, setInput] = useState<string>('');
+
+    return (
+        <div className='m-2 flex bg-sky-900/80 overflow-hidden rounded-md'>
+            <input
+                className='flex-1 px-2 bg-transparent'
+                onKeyDown={(e) => e.key === 'Enter' ? onSubmit(input) : null}
+                value={input} onChange={(e) => setInput(e.target.value)}
+                placeholder='Type a title...'
+            />
+            <button
+                onClick={() => onSubmit(input)}
+            >
+                Search
+            </button>
+        </div>
+    )
+}
+
 export default function PostsPage() {
 
     const [posts, setPosts] = useState<IPost[]>([]);
@@ -21,12 +45,18 @@ export default function PostsPage() {
         setPosts(res.data.posts);
     }
 
+    async function searchPost(params: string) {
+        const res = await axios.get(API_URL + '/posts/search?q=' + params);
+        setPosts(res.data.posts);
+    }
+
     useEffect(() => {
         getPosts();
     }, []);
 
     return (
-        <div>
+        <div className='flex-1 space-y-4'>
+            <SearchBar onSubmit={searchPost}/>
             <PostsList posts={posts}/>
         </div>
     )
--- frontend/src/pages/RootLayout.tsx
+++ frontend/src/pages/RootLayout.tsx
@@ -2,6 +2,6 @@ import { Outlet } from 'react-router-dom'
 
 export default function RootLayout() {
   return (
-    <Outlet/>
+      <Outlet/>
   )
 }
