107ff8cfc0873bff57d591c13f8de2344fdc75b6
Valentín <61363314+Valo21@users.noreply.github.com>
2024-07-10 00:03:00 -0300

    feat: :sparkles: add comment section for posts
    Related to #1. it loads a list of comments from a post and renders it with custom components. It also includes a text area to publish a comment. Not functionality yet.

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,4 +1,7 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 
+const API_URL: string = 'https://dummyjson.com';
 
 export interface IPost {
     id: number
@@ -13,27 +16,81 @@ export interface IPost {
     userId: number
 }
 
+export interface IComment {
+    id: number
+    body: string
+    postId: number
+    likes: number
+    user: {
+        id: number
+        username: string
+        fullName: string
+    }
+}
+
+function CommentInput() {
+    return (
+        <div className='flex flex-col space-y-1 rounded-sm overflow-hidden'>
+            <textarea placeholder='Say something...'/>
+            <button className='w-fit'>Publish</button>
+        </div>
+    )
+}
+
+function Comment({ data }: { data: IComment}) {
+    return (
+        <div className='flex flex-col'>
+            <h2 className='font-bold'>{data.user.fullName}</h2>
+            <p>{data.body}</p>
+            <p className='font-semibold cursor-pointer'>Reply</p>
+        </div>
+    )
+}
+
+function CommentSection({ comments }: { comments: IComment[] }) {
+
+    return (
+        <div className='space-y-4 p-2 bg-slate-800/60 rounded-sm'>
+            <CommentInput/>
+            {comments.map((comment) => <Comment data={comment} />)}
+        </div>
+    )
+}
+
 export default function Post({ data }: {data: IPost}) {
-  return (
-    <div className='min-w-96 p-4 space-y-4 bg-slate-700 rounded-sm'>
-        <h2 className='font-bold'>{data.title}</h2>
-        <p>{data.body}</p>
-        <div className='space-x-2'>
-            {data.tags.map((tag) => <span>#{tag}</span>)}
-        </div>
-        <div className='flex justify-between'>
-            <div className='flex space-x-4'>
-                <div>
-                    👍🏻{data.reactions.likes}
+    const [comments, setComments] = useState<IComment[]>([]);
+
+    async function getComments() {
+        const res = await axios.get(API_URL + '/comments/post/' + data.id);
+        setComments(res.data.comments);
+    }
+
+    return (
+        <div className='min-w-96 p-4 space-y-4 bg-slate-700 rounded-sm'>
+            <h2 className='font-bold'>{data.title}</h2>
+            <p>{data.body}</p>
+            <div className='space-x-2'>
+                {data.tags.map((tag) => <span>#{tag}</span>)}
+            </div>
+            <div className='flex justify-between'>
+                <div className='flex space-x-4'>
+                    <div>
+                        👍🏻{data.reactions.likes}
+                    </div>
+                    <div>
+                        👎🏻{data.reactions.dislikes}
+                    </div>
                 </div>
                 <div>
-                    👎🏻{data.reactions.dislikes}
+                    🙍🏻‍♂️{data.views}
                 </div>
             </div>
-            <div>
-                🙍🏻‍♂️{data.views}
-            </div>
+            {
+                comments.length > 0 ? 
+                <CommentSection comments={comments}/>
+                :
+                <p className='w-fit font-semibold cursor-pointer' onClick={() => getComments()}>Comments...</p>
+            }
         </div>
-    </div>
-  )
+    )
 }
